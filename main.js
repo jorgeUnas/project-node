7b4e0eccf1271afab60cb3cd7a8559dbd08e19fb
EC2 Default User <ec2-user@ip-172-31-40-166.eu-north-1.compute.internal>
2024-05-10 10:08:41 +0000

    Create a constructor and an instance for Field class

---
+++ main.js
@@ -0,0 +1,21 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+  constructor(field){
+    this.field = field;
+  }
+
+}
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+
